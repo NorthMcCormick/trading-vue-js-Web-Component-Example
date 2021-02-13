28c2b95e53df60f6267f8871e70031e556f4bfb2
North McCormick <northmccormick@icloud.com>
2021-02-12 20:26:50 -0800

    [data] feat: make chart a prop so we can update it from the outside, add night mode, add overlays

--- src/components/TradingVueWc.vue
+++ src/components/TradingVueWc.vue
@@ -4,13 +4,16 @@
 <trading-vue ref="chartPrimary" :data="chart" :width="this.width" :height="this.height"
     :color-back="colors.colorBack"
     :color-grid="colors.colorGrid"
-    :color-text="colors.colorText">
+    :color-text="colors.colorText"
+    :overlays="overlays"
+    :night="night">
 </trading-vue>
 </template>
 
 <script>
 import TradingVue from 'trading-vue-js';
-import Data from './data.json';
+
+import Overlays from 'tvjs-overlays'
 
 export default {
     name: 'TradingVueWc',
@@ -32,17 +35,30 @@ export default {
     destroyed () {
         delete this.ro;
     },
-    data() {
-        return {
-            chart: Data,
-            width:  0,
-            height:  0,
-            ro: null,
-            colors: {
+    computed: {
+        colors() {
+            return this.night ? {} : {
                 colorBack: '#fff',
                 colorGrid: '#eee',
-                colorText: '#333',
+                colorText: '#333'
+            }
+        },
+    },
+    props: {
+        chart: {
+            default: function() {
+                return {};
             },
+
+        }
+    },
+    data() {
+        return {
+            width:  0,
+            height:  0,
+            night: true,
+            ro: null,
+            overlays: Object.values(Overlays)
         }
     }
 }
